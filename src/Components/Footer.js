83e4192620f62419828634dcaf5fec85b347eb07
Adish Jain <adish12012002@gmail.com>
2023-10-17 14:14:33 +0530

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { links } from '../data'
+import React from 'react';
+import { socialinks } from '../data';
+import { links } from '../data';
+
 const Footer = () => {
   return (
     <footer className="section footer">
       <ul className="footer-links">
       {links.map((ele)=>{
-            return <li key={ele.id}>
-              <a href={ele.href} className="nav-link"> {ele.text} </a>
-            </li>
+            return (<li key={ele.id}>
+              <a href={ele.href} className="footer-link"> {ele.text} </a>
+            </li>)
           })}
       </ul>
       <ul className="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="footer-icon"
-            ><i className="fab fa-facebook"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="footer-icon"
-            ><i className="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="footer-icon"
-            ><i className="fab fa-squarespace"></i
-          ></a>
-        </li>
+      {socialinks.map((link) => {
+                            const { id, href, icon } = link;
+                            return (
+                                <li key={id}>
+                                    <a href={href} target="_blank"  className="footer-icon" rel="noreferrer"
+                                    ><i className={icon}></i></a>
+
+                                </li>
+                            );
+                        })}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   )
